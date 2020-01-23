1bbb42f8405ecc7dbe17de918ebb61694f83d6f2
V-Knight1 <syedfi@sheridancollege.ca>
2020-01-23 10:12:04 -0500

    Student Code - Git Repository Setup

--- src/thursdayexample/ThursdayExample.java
+++ src/thursdayexample/ThursdayExample.java
@@ -5,6 +5,7 @@ public class ThursdayExample {
     public static void main(String[] args) {
         
         System.out.println("learning github");
+        System.out.println("learning pull operation");
         
     }
     
