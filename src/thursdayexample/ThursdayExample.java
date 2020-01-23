ff177a56502e745da7db22351b7e14075601d484
V-Knight1 <syedfi@sheridancollege.ca>
2020-01-23 10:30:46 -0500

    Merge origin/master
    Conflicts:
    	src/thursdayexample/ThursdayExample.java

--- src/thursdayexample/ThursdayExample.java
+++ src/thursdayexample/ThursdayExample.java
@@ -3,15 +3,11 @@ package thursdayexample;
 public class ThursdayExample {
 
     public static void main(String[] args) {
-        
+        /* Thursday, Jan 23, 2020*/
         System.out.println("learning github");
         System.out.println("learning pull operation");
         System.out.println("learning fetch operation");
         System.out.println("learning merge operation");
-        
-    }
-    
-}
         
     }
     
